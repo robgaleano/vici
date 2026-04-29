c06099322f1d46cdde267c803187bc55d82b0b09
Robert Galeano <galeano92@gmail.com>
2026-04-29 14:05:12 +0200

    refactor(app): monorepo architecture

--- lib/types/index.ts
+++
@@ -1,2 +0,0 @@
-export type { ApiResponse, Nullable, PaginatedResult } from './api';
-export type { PublicUser, User, UserPatch } from './user';
--- lib/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: string;
-  email: string;
-  username: string | null;
-}
-
-export type PublicUser = Pick<User, 'id' | 'username'>;
-
-export type UserPatch = Partial<Pick<User, 'email' | 'username'>>;
